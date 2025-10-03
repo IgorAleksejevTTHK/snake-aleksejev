6028aa7c16514a6e63920f4f118fff9d611b2df5
IgorAleksejevTTHK <igoralekseje@gmail.com>
2025-10-03 10:18:02 +0300

    Add files via upload

--- Snake.cs
+++ Snake.cs
@@ -55,15 +55,16 @@ namespace Snake
 
         public void HandleKey(ConsoleKey key)
         {
-            if (key == ConsoleKey.LeftArrow)
+            if (key == ConsoleKey.LeftArrow && direction != Direction.RIGHT)
                 direction = Direction.LEFT;
-            else if (key == ConsoleKey.RightArrow)
+            else if (key == ConsoleKey.RightArrow && direction != Direction.LEFT)
                 direction = Direction.RIGHT;
-            else if (key == ConsoleKey.DownArrow)
-                direction = Direction.DOWN;
-            else if (key == ConsoleKey.UpArrow)
+            else if (key == ConsoleKey.UpArrow && direction != Direction.DOWN)
                 direction = Direction.UP;
+            else if (key == ConsoleKey.DownArrow && direction != Direction.UP)
+                direction = Direction.DOWN;
         }
+
 
         public bool Eat(Point food)
         {
